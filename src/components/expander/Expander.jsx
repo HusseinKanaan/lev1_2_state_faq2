ac5cadd1330343ad9b1d0dcd56b2b172008fd3f3
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-09-11 19:58:01 +0200

    change

---
+++ src/components/expander/Expander.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './Expander.css'
+
+const Expander = () => {
+    const [big, setBig] = useState(false)
+    const [bigger, setBigger] = useState(false)
+
+    const expand = () => {
+        setBig(!big)
+    }
+
+    const expands = () => {
+        setBigger(!bigger)
+    }
+
+
+    
+    return (
+        <section>
+
+            <article className='question'>
+                <p>Why is React great?</p>
+                <button onClick={expand}>{!big ? '+' : '-' }</button>
+            </article>
+
+            <article className='answer' style={!big ? { display: 'none' } : { display: 'Block' }}>
+                <p>Fast Learning Curve</p>
+                <button onClick={expands}>{!bigger ? '+' : '-'}</button>
+            </article>
+
+            <article className='longanswer' style={!bigger ? { display: 'none' } : { display: 'Block' }}>
+                <p>
+                    React is very a simple and lightweight library that only deals with the view layer. It is not a beast like other MV* frameworks such as Angular or Ember. Any Javascript developer can understand the basics and start developing an awesome web application after only a couple of days reading tutorial.
+                    As the React guide says 'Thinking in React' may be a little different than you used to since it brings a new approach to the table, but it will become much easier and natural as you gain experience with it.
+                </p>
+            </article>
+
+        </section>
+
+)
+
+}
+export default Expander
